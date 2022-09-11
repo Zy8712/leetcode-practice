860b7871bec321f50be2308a4a876deca452fcf3
Zy8712 <76718507+Zy8712@users.noreply.github.com>
2022-09-11 18:25:58 -0400

    Finished First Problem

---
+++ Q1_Two_Sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int [] indices = new int[2];
+        int indicator = -1;
+        for (int i=0; i < nums.length; i++){
+            for (int j=i+1; j < nums.length; j++){
+                if ((nums[i]+nums[j]) == target){
+                    indices[0]=i;
+                    indices[1]=j;
+                    indicator=1;
+                    break;
+                }
+            }
+            if (indicator == 1){
+                break;
+            }
+        }
+        return indices;
+    }
+}
